5e9d65847ed68805b2ef772880469609f18c7457
Joe Cserko <j.cserko@gmail.com>
2021-08-16 10:08:10 +0200

    feat(build)

--- backend/src/seed/seeder.js
+++ backend/src/seed/seeder.js
@@ -2,7 +2,7 @@ const fsp = require('fs').promises;
 const ProductModel = require('../models/product.model');
 
 (async () => {
-    const productsJson = await fsp.readFile('./products.json', 'utf8');
+    const productsJson = await fsp.readFile('src/seed/products.json', 'utf8');
     const products = JSON.parse(productsJson);
     await ProductModel.insertMany(products);
 })();
